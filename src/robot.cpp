5779df1883e1d76150c5cf1d939f75b039695019
Radmonkt <551310554@qq.com>
2025-06-12 13:52:46 +0800

    balance

--- src/robot.cpp
+++ src/robot.cpp
@@ -9,7 +9,7 @@ motorsparam motorsParam;
 
 float roll_kp = 0.0025, roll_kd = -0.007;  //机器人自稳Kp、Kd值
 
-float leftY = 140, rightY = 140;  //机器人Y轴方向腿部高度
+float leftY = 120, rightY = 150;  //机器人Y轴方向腿部高度
 float leftX = 60, rightX = 60;    //机器人X轴方向腿部高度
 float x1 = leftX, x2 = rightX, Y1 = leftY, y2 = rightY;
 float motorLeftFront, motorLeftRear, motorRightFront, motorRightRear;
@@ -179,12 +179,11 @@ void inverseKinematics()
 
   // 定义坐标原点alphaRight
   // 加1.57*8betaRight 
-  motorRightRear = (1.47 + 1.57 * 8) - (IKParam.betaRight * 8);
-  motorRightFront = (3.04 + 1.57 * 8) - (IKParam.alphaRight * 8);
-  // alphaLeft
+  motorRightRear = (2.48 + 1.57 * 8) - (IKParam.betaRight * 8);
+  motorRightFront = (1.07 + 1.57 * 8) - (IKParam.alphaRight * 8);  // alphaLeft
   
-  motorLeftRear = (2.46 + 1.57 * 8) - (IKParam.betaLeft * 8);
-  motorLeftFront = (4.73 + 1.57 * 8) - (IKParam.alphaLeft * 8);
+  motorLeftRear = (1.27 + 1.57 * 8) - (IKParam.betaLeft * 8);
+  motorLeftFront = (0.96 + 1.57 * 8) - (IKParam.alphaLeft * 8);
 }
 
 
